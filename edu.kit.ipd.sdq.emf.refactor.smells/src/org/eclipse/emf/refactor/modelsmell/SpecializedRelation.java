276381b422b15f59a7c0bb4f9b21426138a4eb2f
Misha Strittmatter <strittmatter@kit.edu>
2019-02-22 13:19:58 +0100

    improved SpecializedRelation smell #14
    detailed reporting (owner,target), detect multiple occurrences on one
    class, ignore references to the same class

--- edu.kit.ipd.sdq.emf.refactor.smells/src/org/eclipse/emf/refactor/modelsmell/SpecializedRelation.java
+++ edu.kit.ipd.sdq.emf.refactor.smells/src/org/eclipse/emf/refactor/modelsmell/SpecializedRelation.java
@@ -1,8 +1,12 @@
 package org.eclipse.emf.refactor.modelsmell;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
@@ -12,88 +16,58 @@ import edu.kit.ipd.sdq.emf.refactor.smells.util.DetectionHelper;
 
 /**
  * This clas investigates if a subclass defines a specialization relation to a already refered type.
+ * 
  * @author renehahn
  *
  */
 public final class SpecializedRelation implements IModelSmellFinder {
 
-	@Override
-	public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
-		LinkedList<LinkedList<EObject>> results = new LinkedList<LinkedList<EObject>>();
-		List<EClass> classes = DetectionHelper.getAllEClasses(root);
-		for(EClass currentClass : classes)
-		{
-			if(hasClassSpecializationAggregation(currentClass))
-			{
-				LinkedList<EObject> result = new LinkedList<EObject>();
-				result.add(currentClass);
-				results.add(result);
-			}
-		}		
-		
-		return results;
-	}	
-	
-	/*private boolean hasClassSpecializationAggregation(EClass currentClass)
-	{
-		for(EReference reference : currentClass.getEAllReferences())
-		{
-			for(EReference localReference: currentClass.getEReferences())
-			{
-				if(reference != localReference)
-				{
-					EClass referenceClass = reference.getEReferenceType();
-					EClass localReferenceClass = localReference.getEReferenceType();
-					
-					if(referenceClass.equals(localReferenceClass))
-					{
-						return true;
-					}
-					
-					if(referenceClass.isSuperTypeOf(localReferenceClass))
-					{
-						return true;
-					}
-				}
-			}
-		}
-		return false;
-	}*/
-	
-	private boolean hasClassSpecializationAggregation(EClass currentClass)
-	{
-		LinkedList<EReference> inheritedReferences = new LinkedList<EReference>();
-		
-		for(EReference reference : currentClass.getEAllReferences())
-		{
-			if(!currentClass.getEReferences().contains(reference))
-			{
-				inheritedReferences.add(reference);
-			}
-		}
-		
-		
-		for(EReference reference : inheritedReferences)
-		{
-			for(EReference localReference: currentClass.getEReferences())
-			{
-				if(reference != localReference)
-				{
-					EClass referenceClass = reference.getEReferenceType();
-					EClass localReferenceClass = localReference.getEReferenceType();
-					
-					if(referenceClass.equals(localReferenceClass))
-					{
-						return true;
-					}
-					
-					if(referenceClass.isSuperTypeOf(localReferenceClass))
-					{
-						return true;
-					}
-				}
-			}
-		}
-		return false;
-	}
+    @Override
+    public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
+        LinkedList<LinkedList<EObject>> results = new LinkedList<LinkedList<EObject>>();
+        List<EClass> classes = DetectionHelper.getAllEClasses(root);
+        for (EClass currentClass : classes) {
+            findClassSpecializationAggregation(currentClass, results);
+        }
+        return results;
+    }
+
+    private void findClassSpecializationAggregation(EClass currentClass, LinkedList<LinkedList<EObject>> results) {
+
+        EList<EReference> localReferences = currentClass.getEReferences();
+
+        // iterate all inherited references
+        Set<EReference> inheritedReferences = getInheritedReferences(currentClass);
+        for (EReference inheritedReference : inheritedReferences) {
+
+            // iterate all local references
+            for (EReference localReference : localReferences) {
+                if (inheritedReference != localReference) {
+
+                    // is local reference target a subclass of the inherited class? 
+                    EClass inheritedReferenceTarget = inheritedReference.getEReferenceType();
+                    EClass localReferenceTarget = localReference.getEReferenceType();
+                    if (inheritedReferenceTarget.isSuperTypeOf(localReferenceTarget) && !inheritedReferenceTarget.equals(localReferenceTarget)) {
+
+                        // occurrence found
+                        LinkedList<EObject> result = new LinkedList<EObject>();
+                        result.add(currentClass);
+                        result.add(localReferenceTarget);
+                        results.add(result);
+                    }
+                }
+            }
+        }
+    }
+
+    private Set<EReference> getInheritedReferences(EClass currentClass) {
+        Collection<EReference> ownReferences = currentClass.getEReferences();
+        HashSet<EReference> inheritedReferences = new HashSet<>();
+        for (EReference reference : currentClass.getEAllReferences()) {
+            if (!ownReferences.contains(reference)) {
+                inheritedReferences.add(reference);
+            }
+        }
+        return inheritedReferences;
+    }
 }
