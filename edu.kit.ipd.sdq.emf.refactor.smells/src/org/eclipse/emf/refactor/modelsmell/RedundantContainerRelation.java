f818015bfe5b30d32ad61fb79db0cc3a0fd3485c
Misha Strittmatter <strittmatter@kit.edu>
2019-02-22 09:41:03 +0100

    improved detection: report owner of container relation and the target (owner,target)

--- edu.kit.ipd.sdq.emf.refactor.smells/src/org/eclipse/emf/refactor/modelsmell/RedundantContainerRelation.java
+++ edu.kit.ipd.sdq.emf.refactor.smells/src/org/eclipse/emf/refactor/modelsmell/RedundantContainerRelation.java
@@ -10,38 +10,42 @@ import org.eclipse.emf.refactor.smells.interfaces.IModelSmellFinder;
 
 import edu.kit.ipd.sdq.emf.refactor.smells.util.DetectionHelper;
 
-
 public final class RedundantContainerRelation implements IModelSmellFinder {
 
-	@Override
-	public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
-		LinkedList<LinkedList<EObject>> results = new LinkedList<LinkedList<EObject>>();
-		List<EClass> classes = DetectionHelper.getAllEClasses(root);
-		for(EClass currentClass : classes)
-		{
-			if(hasRedundantContainerRelation(currentClass))
-			{
-				LinkedList<EObject> result = new LinkedList<EObject>();
-				result.add(currentClass);
-				results.add(result);
-			}
-		}		
-		
-		return results;
-	}
-	
-	private boolean hasRedundantContainerRelation(EClass currentClass)
-	{
-		for(EReference reference : currentClass.getEReferences())
-		{
-			if(reference.isContainment())
-			{
-				if(reference.getEOpposite() != null)
-				{
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    @Override
+    public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
+        LinkedList<LinkedList<EObject>> results = new LinkedList<LinkedList<EObject>>();
+        List<EClass> classes = DetectionHelper.getAllEClasses(root);
+        for (EClass currentClass : classes) {
+            findRedundantContainerRelations(currentClass, results);
+        }
+
+        return results;
+    }
+
+    private void findRedundantContainerRelations(EClass currentClass, LinkedList<LinkedList<EObject>> results) {
+        for (EReference reference : currentClass.getEReferences()) {
+            if (reference.isContainer()) {
+                LinkedList<EObject> result = new LinkedList<EObject>();
+                result.add(currentClass);
+                result.add(reference.getEType());
+                results.add(result);
+            }
+        }
+    }
+
+//    private boolean hasRedundantContainerRelation(EClass currentClass)
+//	{
+//		for(EReference reference : currentClass.getEReferences())
+//		{
+//			if(reference.isContainment())
+//			{
+//				if(reference.getEOpposite() != null)
+//				{
+//					return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
 }
