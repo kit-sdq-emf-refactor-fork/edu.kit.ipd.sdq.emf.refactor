b2f8c5c079bb204c5475c7ec22a3e8ed13c79e39
Misha Strittmatter <strittmatter@kit.edu>
2019-02-25 11:37:39 +0100

    improved concrete abstract class detection #14
    result now container concrete superclass with all its abstract
    subclasses. no more duplicates

--- edu.kit.ipd.sdq.emf.refactor.smells/src/org/eclipse/emf/refactor/modelsmell/ConcreteAbstractClass.java
+++ edu.kit.ipd.sdq.emf.refactor.smells/src/org/eclipse/emf/refactor/modelsmell/ConcreteAbstractClass.java
@@ -2,51 +2,52 @@ package org.eclipse.emf.refactor.modelsmell;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.refactor.smells.interfaces.IModelSmellFinder;
+import org.eclipse.emf.refactor.smells.runtime.core.ModelSmellFinder;
+import org.jgrapht.graph.AsSubgraph;
+import org.jgrapht.graph.DefaultEdge;
 
+import edu.kit.ipd.sdq.ecoregraph.util.EcoreGraphUtil;
 import edu.kit.ipd.sdq.emf.refactor.smells.util.DetectionHelper;
 
 /**
  * This class checks if a hierarchy is realized too late.
+ * 
  * @author renehahn
  *
  */
 public final class ConcreteAbstractClass implements IModelSmellFinder {
 
-	@Override
-	public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
-		LinkedList<LinkedList<EObject>> results = new LinkedList<>();
-		List<EClass> classes = DetectionHelper.getAllEClasses(root);
-		
-		for(EClass currentClass : classes)
-		{
-			if(hasClassLateHierarchy(currentClass))
-			{
-				LinkedList<EObject> result = new LinkedList<>();
-				result.add(currentClass);
-				results.add(result);
-			}
-		}
-		
-		return results;
-	}
-	
-	private boolean hasClassLateHierarchy(EClass currentClass)
-	{
-		if(currentClass.isAbstract())
-		{
-			for(EClass superType : currentClass.getESuperTypes())
-			{
-				if(!superType.isAbstract())
-				{
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
+    @Override
+    public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
+
+        AsSubgraph<EClassifier, DefaultEdge> hierarchyGraph = EcoreGraphUtil.hierarchySubGraph(ModelSmellFinder.ecoreGraph);
+
+        LinkedList<LinkedList<EObject>> results = new LinkedList<>();
+
+        // iterate all classes
+        List<EClass> classes = DetectionHelper.getAllEClasses(root);
+        for (EClass eClass : classes) {
+
+            // is class concrete?
+            if (!eClass.isAbstract()) {
+
+                // class has abstract subclasses?
+                Set<EClass> abstractSubclasses = EcoreGraphUtil.getAbstractSubclasses(eClass, hierarchyGraph);
+                if (abstractSubclasses.size() > 0) {
+                    LinkedList<EObject> result = new LinkedList<>();
+                    result.add(eClass);
+                    result.addAll(abstractSubclasses);
+                    results.add(result);
+                }
+            }
+        }
+
+        return results;
+    }
 }
