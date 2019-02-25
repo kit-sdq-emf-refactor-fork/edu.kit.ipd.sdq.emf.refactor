371ac968d0b1842b621eff0e5867bea2c87080ae
Misha Strittmatter <strittmatter@kit.edu>
2019-02-25 11:33:27 +0100

    new helper

--- edu.kit.ipd.sdq.ecoregraph/src/edu/kit/ipd/sdq/ecoregraph/util/EcoreGraphUtil.java
+++ edu.kit.ipd.sdq.ecoregraph/src/edu/kit/ipd/sdq/ecoregraph/util/EcoreGraphUtil.java
@@ -16,74 +16,65 @@ import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 
 import edu.kit.ipd.sdq.ecoregraph.Dependency;
-import edu.kit.ipd.sdq.ecoregraph.EcoreGraph;
 import edu.kit.ipd.sdq.ecoregraph.Dependency.DependencyType;
-
+import edu.kit.ipd.sdq.ecoregraph.EcoreGraph;
 
 public class EcoreGraphUtil {
-	
-	public static Collection<List<EClassifier>> findSimpleCycles(EcoreGraph eGraph) {
-		DefaultDirectedGraph<EClassifier, DefaultEdge> graph = eGraph.getGraph();
-		
-		JohnsonSimpleCycles<EClassifier, DefaultEdge> cycleDetector = new JohnsonSimpleCycles<EClassifier, DefaultEdge>(graph);
-		Collection<List<EClassifier>> cycles = cycleDetector.findSimpleCycles();
-		
-		return cycles;
-	}
-	
-	public static AsSubgraph<EClassifier, DefaultEdge> hierarchySubGraph(EcoreGraph eGraph) {
-		DefaultDirectedGraph<EClassifier, DefaultEdge> graph = eGraph.getGraph();
-		Map<EClassifier, Set<Dependency>> dependencies = eGraph.getDependencies();
 
-		Set<EClass> vertexSubset = graph.vertexSet().stream().filter(x -> x instanceof EClass).map(x -> (EClass) x)
-				.collect(Collectors.toSet());
-		Set<DefaultEdge> edgeSubset = new HashSet<DefaultEdge>();
+    public static Collection<List<EClassifier>> findSimpleCycles(EcoreGraph eGraph) {
+        DefaultDirectedGraph<EClassifier, DefaultEdge> graph = eGraph.getGraph();
 
+        JohnsonSimpleCycles<EClassifier, DefaultEdge> cycleDetector = new JohnsonSimpleCycles<EClassifier, DefaultEdge>(graph);
+        Collection<List<EClassifier>> cycles = cycleDetector.findSimpleCycles();
 
-		for (EClass c : vertexSubset) {
-			Set<DefaultEdge> incomingEdges = graph.incomingEdgesOf(c);
-			Set<DefaultEdge> allowedIncomingEdges = incomingEdges.stream()
-					.filter(e -> dependencies.get(graph.getEdgeSource(e)).stream()
-							.anyMatch(d -> d.getTarget() == c && d.getType().equals(DependencyType.E_SUPER_TYPE)))
-					.collect(Collectors.toSet());
-			edgeSubset.addAll(allowedIncomingEdges);
+        return cycles;
+    }
 
-			Set<DefaultEdge> outgoingEdges = graph.outgoingEdgesOf(c);
-			Set<DefaultEdge> allowedOutgoingEdges = outgoingEdges
-					.stream().filter(
-							e -> dependencies.get(c).stream()
-									.anyMatch(d -> d.getTarget() == graph.getEdgeTarget(e)
-											&& d.getType().equals(DependencyType.E_SUPER_TYPE)))
-					.collect(Collectors.toSet());
-			edgeSubset.addAll(allowedOutgoingEdges);
+    public static AsSubgraph<EClassifier, DefaultEdge> hierarchySubGraph(EcoreGraph eGraph) {
+        DefaultDirectedGraph<EClassifier, DefaultEdge> graph = eGraph.getGraph();
+        Map<EClassifier, Set<Dependency>> dependencies = eGraph.getDependencies();
 
+        Set<EClass> vertexSubset = graph.vertexSet().stream().filter(x -> x instanceof EClass).map(x -> (EClass) x).collect(Collectors.toSet());
+        Set<DefaultEdge> edgeSubset = new HashSet<DefaultEdge>();
 
-		}
+        for (EClass c : vertexSubset) {
+            Set<DefaultEdge> incomingEdges = graph.incomingEdgesOf(c);
+            Set<DefaultEdge> allowedIncomingEdges = incomingEdges.stream()
+                    .filter(e -> dependencies.get(graph.getEdgeSource(e)).stream().anyMatch(d -> d.getTarget() == c && d.getType().equals(DependencyType.E_SUPER_TYPE))).collect(Collectors.toSet());
+            edgeSubset.addAll(allowedIncomingEdges);
 
-		AsSubgraph<EClassifier, DefaultEdge> asSubgraph = new AsSubgraph<EClassifier, DefaultEdge>(graph, vertexSubset,
-				edgeSubset);
+            Set<DefaultEdge> outgoingEdges = graph.outgoingEdgesOf(c);
+            Set<DefaultEdge> allowedOutgoingEdges = outgoingEdges.stream()
+                    .filter(e -> dependencies.get(c).stream().anyMatch(d -> d.getTarget() == graph.getEdgeTarget(e) && d.getType().equals(DependencyType.E_SUPER_TYPE))).collect(Collectors.toSet());
+            edgeSubset.addAll(allowedOutgoingEdges);
 
-		return asSubgraph;
-	}
-	
-	public static void printGraph(Graph<EClassifier, DefaultEdge> graph) {
-    	System.out.println("---");
-		System.out.println("Vertices:");
-    	System.out.println("---");
-    	for (EClassifier v : graph.vertexSet()) {
-    		System.out.println(v.getName());
-    	}
-    	System.out.println("---");
-    	for (DefaultEdge e : graph.edgeSet()) {
-    		System.out.println(graph.getEdgeSource(e).getName() + "=>" + graph.getEdgeTarget(e).getName());
-    	}
-    	System.out.println("---");    		
-	}
+        }
+
+        AsSubgraph<EClassifier, DefaultEdge> asSubgraph = new AsSubgraph<EClassifier, DefaultEdge>(graph, vertexSubset, edgeSubset);
+
+        return asSubgraph;
+    }
+
+    public static void printGraph(Graph<EClassifier, DefaultEdge> graph) {
+        System.out.println("---");
+        System.out.println("Vertices:");
+        System.out.println("---");
+        for (EClassifier v : graph.vertexSet()) {
+            System.out.println(v.getName());
+        }
+        System.out.println("---");
+        for (DefaultEdge e : graph.edgeSet()) {
+            System.out.println(graph.getEdgeSource(e).getName() + "=>" + graph.getEdgeTarget(e).getName());
+        }
+        System.out.println("---");
+    }
 
     public static Set<EClass> getSubclasses(EClass eClass, AsSubgraph<EClassifier, DefaultEdge> hierarchyGraph) {
-        Set<DefaultEdge> incomingEdgesToDestination = hierarchyGraph.incomingEdgesOf(eClass);
-        Set<EClass> subclassesOfDestination = incomingEdgesToDestination.stream().map(e -> (EClass) hierarchyGraph.getEdgeSource(e)).collect(Collectors.toSet());
-        return subclassesOfDestination;
+        return hierarchyGraph.incomingEdgesOf(eClass).stream().map(e -> (EClass) hierarchyGraph.getEdgeSource(e)).collect(Collectors.toSet());
     }
-	
+
+    public static Set<EClass> getAbstractSubclasses(EClass eClass, AsSubgraph<EClassifier, DefaultEdge> hierarchyGraph) {
+        return hierarchyGraph.incomingEdgesOf(eClass).stream().map(e -> (EClass) hierarchyGraph.getEdgeSource(e)).filter(EClass::isAbstract).collect(Collectors.toSet());
+    }
+
 }
